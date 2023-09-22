09b0a01a3cb4fa26a18fe9c71fb8142fb5a76bbb
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-09-22 19:35:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,68 +1,52 @@
+// required packages
 const express = require('express');
 const fetch = require('node-fetch');
 require('dotenv').config();
-const fs = require('fs');
-const path = require('path');
 
+// creating the express server
 const app = express();
+
+// server port number
 const PORT = process.env.PORT || 3000;
 
+// set template engine
+app.set('view engine', 'ejs');
 app.use(express.static("public"));
-app.use('/assets', express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+app.use('/assets', express.static("public"))
+
+// needed to parse html data POST request
+app.use(express.urlencoded({
+    extended: true
+}))
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.render('index');
-});
+})
 
-app.get('/download/:videoID', async (req, res) => {
-    const videoID = req.params.videoID;
-    
-    if (!videoID) {
-        return res.status(400).send('Video ID not provided.');
-    }
-
-    try {
+app.get('/convert-mp3', async (req, res) => {
+    const videoID = req.body.videoID;
+    if (videoID === undefined || videoID === '' || videoID === null) {
+        return res.render('index', { success: false, message: 'Please enter a video ID' });
+    } else {
         const fetchAPI = await fetch(`https://youtube-mp36.p.rapidapi.com/dl?id=${videoID}`, {
-            method: 'GET',
-            headers: {
+            'method': 'GET',
+            'headers': {
                 'x-rapidapi-key': process.env.API_KEY,
                 'x-rapidapi-host': process.env.API_HOST
             }
-        });
+        })
 
         const fetchResponse = await fetchAPI.json();
 
-        if (fetchResponse.status === 'ok') {
-            const fileURL = fetchResponse.link;
-            const fileName = fetchResponse.title + '.mp3';
-            const filePath = path.join(__dirname, 'downloads', fileName);
-
-            const fileStream = fs.createWriteStream(filePath);
-
-            const downloadResponse = await fetch(fileURL);
-            downloadResponse.body.pipe(fileStream);
-
-            downloadResponse.body.on('end', () => {
-                res.download(filePath, fileName, (err) => {
-                    if (err) {
-                        console.error(err);
-                        res.status(500).send('Error during download.');
-                    }
-                    fs.unlinkSync(filePath);
-                });
-            });
-        } else {
-            res.status(400).send('Error: ' + fetchResponse.msg);
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal server error.');
+        if (fetchResponse.status === 'ok')
+            return res.render('index', { success: true, song_title: fetchResponse.title, song_link: fetchResponse.link });
+        else
+            return res.render('index', { success: false, song_title: fetchResponse.title, message: fetchResponse.msg });
     }
-});
+})
 
+// starting the server
 app.listen(PORT, () => {
-    console.log('Server on port 3000');
+    console.log('Server on port 3000')
 });
-      
